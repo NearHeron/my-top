898e28f9f6557278796e737680891f6f0d82fbdc
Daniil Tsyplyackov <DTsyplyackov@yandex.ru>
2021-09-04 17:31:23 +0300

    controllers

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -6,19 +6,20 @@ export enum TopLevelCategory {
 }
 
 export class TopPageModel {
+  _id: string;
   firstLevelCategory: TopLevelCategory;
   secondLevelCategory: string;
   title: string;
   category: string;
   hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
+	  count: number;
+	  juniorSalary: number;
+	  middleSalary: number;
+	  seniorSalary: number;
   };
   advantages: {
-    title: string;
-    description: string;
+	title: string;
+	description: string;
   }[];
   seoText: string;
   tagsTitle?: string;
